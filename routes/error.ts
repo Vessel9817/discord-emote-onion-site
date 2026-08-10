658b40db0f0065e977ea9107a5881267fb69ff1d
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-08-10 07:28:29 -0400

    chore: remove unused router
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- routes/error.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { errorHandler } from '../middleware/index';
-
-const router = express.Router();
-
-router.use('/', errorHandler);
-
-export default router;
--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,7 @@
 import appRouter from './app';
-import errorRouter from './error';
 import notFoundRouter from './notFound';
 
 export {
     appRouter,
-    errorRouter,
     notFoundRouter
 };
