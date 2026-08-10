3d74b41d768c2c7d79494d9a2bfe29ab5b3110e7
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-08-10 07:28:15 -0400

    chore: match casing
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- app.ts
+++ app.ts
@@ -2,22 +2,22 @@ import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
 import { emotes } from './env';
-import { APP_ROUTER, NOT_FOUND_ROUTER } from './routes/index';
+import { appRouter, notFoundRouter } from './routes/index';
 
-const APP = express();
-const PORT = 3000;
+const app = express();
+const port = 3000;
 
-APP.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // Removing fingerprintable headers
-APP.disable('x-powered-by');
+app.disable('x-powered-by');
 
 // Setting global app middleware
-APP.use(cors()); // Sets CORS policy
-APP.use(express.json()); // Parse Content-Type: json
-APP.use(express.urlencoded({ extended: false })); // Encodes special characters in URLs
-APP.use('/', APP_ROUTER); // Serves app
-APP.use('/', NOT_FOUND_ROUTER); // Catches errors
+app.use(cors()); // Sets CORS policy
+app.use(express.json()); // Parse Content-Type: json
+app.use(express.urlencoded({ extended: false })); // Encodes special characters in URLs
+app.use('/', appRouter); // Serves app
+app.use('/', notFoundRouter); // Catches errors
 
 // Starting server
 try {
@@ -27,6 +27,6 @@ catch (err) {
     console.error('Failed to connect to database:', err);
 }
 
-APP.listen(PORT, () => {
+app.listen(port, () => {
     console.log('Server is running!');
 });
--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,9 @@
-import APP_ROUTER from './app';
-import ERROR_ROUTER from './error';
-import NOT_FOUND_ROUTER from './notFound';
+import appRouter from './app';
+import errorRouter from './error';
+import notFoundRouter from './notFound';
 
 export {
-    APP_ROUTER,
-    ERROR_ROUTER,
-    NOT_FOUND_ROUTER
+    appRouter,
+    errorRouter,
+    notFoundRouter
 };
